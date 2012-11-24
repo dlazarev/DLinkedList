b5c39870b2e5ac48f4c859fe3f7a3c6006d9a79e
Dmitry Lazarev <diman.lazarev@gmail.com>
2012-11-24 15:38:26 +0400

    Initial Commit

---
+++ DLinkedList/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DLinkedList
+//
+//  Created by Dmitry Lazarev on 24.11.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
